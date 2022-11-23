caad8d09dff630195e3d4489d7670546162c6721
sanghoy <shyoon@8pin.co.kr>
2022-11-23 16:06:45 +0900

    study_1123

---
+++ zoomProject/semple.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/main')
+def main_page():
+    return "This is main page!!"
+
+if __name__ == '__main__':
+    app.ren(host='자신의 IP',port=5000)
